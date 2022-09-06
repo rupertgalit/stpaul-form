ddbeb215990ac3927cb70117cc631efccb799869
Rupert <galitrupert1522@gmail.com>
2022-09-06 11:37:17 +0800

    🧬 add icon

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>FORM</title>
+  <link rel="icon" type="image/x-icon" href="assets/image/stpaul.ico">
 
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="assets/css/style2.css">
